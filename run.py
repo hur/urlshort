c141c3b9a57076029a6c4b5896edd84d31289ebf
hur <4998544+hur@users.noreply.github.com>
2020-05-08 09:19:28 +0300

    modularised html templates

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 from urlshort import app, db
+from urlshort.strings import Strings
+
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db}
+
+
+@app.context_processor
+def inject_strings():
+    """
+    Inject strings automatically into the context of templates
+    """
+    return Strings.strings
